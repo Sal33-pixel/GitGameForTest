ef042647890c59e35778e14297d29899f30f1320
Sal33-pixel <jackkrikri@gmail.com>
2020-10-06 16:21:22 +0200

    npm reinstall

--- public/js/app.js
+++ public/js/app.js
@@ -15839,8 +15839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/sal33/www/GameFor/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/sal33/www/GameFor/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/sal33/www/GitGameFor/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/sal33/www/GitGameFor/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
